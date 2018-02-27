c142b24acc7585ef11c2d6b5eb9fcf19d97d408b
chase cameron <chase cameron>
2018-02-26 20:34:21 -0800

    Okay, real starting point; Clarified some other thoughts and issues I was having.

--- projects/mcwhirtc/assignment-4/Calendar/src/test/java/calendar/TimeTableRandomTest.java
+++ projects/mcwhirtc/assignment-4/Calendar/src/test/java/calendar/TimeTableRandomTest.java
@@ -2,30 +2,20 @@ package calendar;
 
 import java.util.Calendar;
 import java.util.Random;
-
 import org.junit.Test;
-
-
 import static org.junit.Assert.*;
 
-
-
-/**
- * Random Test Generator  for TimeTable class.
- */
+/*
+** Random Test Generator  for TimeTable class.
+*/
 
 public class TimeTableRandomTest {
-	
-    /**
-     * Generate Random Tests that tests TimeTable Class.
-     */
-	 @Test
-	  public void radnomtest()  throws Throwable  {
-		 
-		 
-		 
-	 }
+    /*
+    ** Generate Random Tests that tests TimeTable Class.
+    */
 
-
+	@Test
+	public void radnomtest()  throws Throwable  {
 	
+	}
 }
